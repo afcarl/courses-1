6659a22b5e6ae2e31a2c8147e44e8cca225ec1dd
Bhargav Chippada <bhargavchippada19@gmail.com>
2017-07-12 18:58:08 +0530

    softmax done

--- cs231n/assignment1/cs231n/classifiers/softmax.py
+++ cs231n/assignment1/cs231n/classifiers/softmax.py
@@ -21,8 +21,26 @@ def softmax_loss_naive(W, X, y, reg):
   - gradient with respect to weights W; an array of same shape as W
   """
   # Initialize the loss and gradient to zero.
+  num_train = X.shape[0]
+  num_classes = W.shape[1]
   loss = 0.0
   dW = np.zeros_like(W)
+  for i in range(num_train):
+        unnormalized_probs = np.e**np.matmul(X[i],W)
+        sum_unnormalized_probs = np.sum(unnormalized_probs)
+        normalized_probs = unnormalized_probs/sum_unnormalized_probs
+        loss+= -1*np.log(normalized_probs[y[i]])
+        for j in range(num_classes):
+            if j == y[i]:
+                grad = (normalized_probs[y[i]]-1)*X[i]
+                dW[:,j] += grad.T
+            else:
+                grad = normalized_probs[j]*X[i]
+                dW[:,j] += grad.T
+  loss += 0.5*reg*np.sum(W*W)
+  loss /= num_train
+  dW += reg*W
+  dW /= num_train
 
   #############################################################################
   # TODO: Compute the softmax loss and its gradient using explicit loops.     #
@@ -45,8 +63,14 @@ def softmax_loss_vectorized(W, X, y, reg):
   Inputs and outputs are the same as softmax_loss_naive.
   """
   # Initialize the loss and gradient to zero.
-  loss = 0.0
-  dW = np.zeros_like(W)
+  num_train = X.shape[0]
+  unnormalized_probs = np.e**np.matmul(X,W)
+  sum_unnormalized_probs = np.sum(unnormalized_probs, axis = 1).reshape(num_train,1)
+  normalized_probs = unnormalized_probs/sum_unnormalized_probs
+  loss = (np.sum(-1*np.log(np.choose(y, normalized_probs.T))) + reg*np.sum(W*W))/num_train
+  grads = normalized_probs
+  grads[np.arange(grads.shape[0]), y] -=1
+  dW = (np.matmul(X.T,grads)+reg*W)/num_train
 
   #############################################################################
   # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
